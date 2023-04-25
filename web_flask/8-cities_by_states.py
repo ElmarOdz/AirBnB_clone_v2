30ad7b133d24718578bd1d29de91ef7edfb17711
ElmarOdz <110916781+ElmarOdz@users.noreply.github.com>
2023-04-25 22:11:26 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
+""" a script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage, State
 
-from flask import Flask, render_template
-import models
-
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def refresh(exception):
-        models.storage.close()
+def remove_session(exception):
+    """ After each request, it removes the current SQLAlchemy Session """
+    storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def route_states():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('7-states_list.html', states_list=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def render_state_cities():
+    """ displays all states and their cities """
+    States = storage.all(State).values()
+    return render_template("8-cities_by_states.html", States=States)
 
-
-@app.route("/cities_by_states", strict_slashes=False)
-def route_city():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('8-cities_by_states.html', states_list=states)
-
-
-if __name__ == "__main__":
-        app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
